28ec3bd77f3a7b447f4a8fe65e39cfc545d9a839
Rusty Roberts <rust.roberts@protonmail.com>
2021-01-21 18:26:24 -0800

    warp noodling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use warp::Filter;
 
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
 #[tokio::main]
 async fn main() {
-    // GET / => 200 OK with body "Hello, Warp!"
-    let hello = warp::path::end().map(|| "Hello, Warp!");
+    pretty_env_logger::init();
+    // GET / => 200 OK with body "Hello, root!"
+    let hello = warp::path::end().map(|| "Hello, root!");
+    // GET /apple => 200 OK with body "Hello, apple!"
+    let hello_friend = warp::path!(String).map(|friend: String| param_extract(friend));
 
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    // API
+    let routes = warp::get().and(hello.or(hello_friend));
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+fn param_extract(param: String) -> String {
+    info!("I extracted {}!", &param);
+    return format!("Hello, {}!", &param);
 }
