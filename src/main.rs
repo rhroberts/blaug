4847a34b7ff33d58c5c2db4192b83a71ddb7cb07
Rusty Roberts <rust.roberts@protonmail.com>
2021-02-21 13:45:21 -0800

    main switched to tera

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-mod blog;
+// mod blog;
 
-use blog::posts;
+// use blog::posts;
 #[macro_use]
 extern crate lazy_static;
-use log;
-use pretty_env_logger;
-use tera::{Context, Tera};
-use warp::{http, Filter};
+use tera::Tera;
+use tide;
+use tide_tera::prelude::*;
 
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
-        let mut tera = match Tera::new("templates/*.html") {
+        let tera = match Tera::new("templates/*.html") {
             Ok(t) => t,
             Err(e) => {
                 println!("Parsing error: {}", e);
@@ -21,41 +20,26 @@ lazy_static! {
     };
 }
 
-fn from_template(
-    template: &str,
-    context: tera::Context,
-) -> http::Result<http::Response<String>> {
-    let resp =
-        http::Response::builder().body(TEMPLATES.render(template, &context).unwrap());
-    return resp;
-}
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    tide::log::start();
 
-#[tokio::main]
-async fn main() {
-    pretty_env_logger::init();
-    // GET / => 200 OK with body "Hello."
-    // let home = warp::path::end().map(|| "Hello.");
-    // let tera = match Tera::new("templates/*.html") {
-    //     Ok(t) => t,
-    //     Err(e) => {
-    //         println!("Parsing error(s): {}", e);
-    //         ::std::process::exit(1);
-    //     }
-    // };
-    let mut context = Context::new();
-    context.insert("test", &100);
-    // let tmp = http::Response::builder().body(TEMPLATES.render("index.html", &context));
-    let tmp = from_template("index.html", context);
-    println!("{}", tmp);
-    let home = warp::path::end().map(|| tmp);
-    // GET /blog
-    let blog = warp::path!("blog").and(warp::fs::file("static/blog/html/index.html"));
-    // GET /blog/[article_title]
-    let posts = warp::path!("blog" / String).map(|post| posts::get_post(post));
-    // GET /static/[asset]
-    let images = warp::path("static").and(warp::fs::dir("static/blog/images"));
-    // API
-    let routes = warp::get().and(home.or(blog).or(posts).or(images));
-    log::info!("Starting server...");
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    let mut tera = Tera::new("templates/*.html")?;
+    tera.autoescape_on(vec!["html"]);
+
+    let mut app = tide::with_state(tera);
+
+    // serve static files
+    app.at("/static").serve_dir("static/")?;
+    // root
+    app.at("/").get(|_| async move {
+        TEMPLATES.render_response(
+            "index.html",
+            &context! {
+                "test" => "Hello from tera template!"
+            },
+        )
+    });
+    app.listen("127.0.0.1:3030").await?;
+    Ok(())
 }
